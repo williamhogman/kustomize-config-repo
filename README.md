4c4e6a136083b153efce03110dd06d38bc97aaa7
whn <me@whn.se>
2020-09-15 23:48:25 +0200

    Initial commit for kustomize-config-repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kustomize-config-repo
 A repository template for Kustomize configuration in a micro service setting.
+
+## How to set it up
+
+```./setup.sh NAME-OF-YOUR-SERVICE```
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+if [ -z "$1" ]
+  then
+    echo "setup.sh name-of-your-service"
+    exit
+fi
+
+sed -i '' "s/your-app/$1/g" base/deployment.yaml
+sed -i '' "s/your-app/$1/g" base/service.yaml
+sed -i '' "s/your-app/$1/g" base/kustomization.yaml
+sed -i '' "s/your-app/$1/g" prod/deployment.yaml
+
+
